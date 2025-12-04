8af343f4534c06038bcc99e3f820ba1794f6c711
Ian <118707480+irgordon@users.noreply.github.com>
2025-12-04 01:31:47 -0500

    Update class-studiofy-settings.php

--- admin/class-studiofy-settings.php
+++ admin/class-studiofy-settings.php
@@ -1,56 +1,31 @@
 <?php
-
-declare(strict_types=1);
-
 class Studiofy_Settings {
-
 	public function __construct() {
-		add_action( 'admin_init', array( $this, 'register_settings' ) );
+		add_action( 'admin_init', array( $this, 'init' ) );
+		add_action( 'admin_menu', array( $this, 'menu' ) );
 	}
-
-	public function register_settings(): void {
-		register_setting( 'studiofy_option_group', 'studiofy_settings', array( $this, 'sanitize_callback' ) );
-
-		add_settings_section( 'studiofy_api', __( 'API Credentials', 'studiofy-crm' ), null, 'studiofy-settings' );
-
-		add_settings_field( 'google_client_id', __( 'Google Client ID', 'studiofy-crm' ), array( $this, 'field_text' ), 'studiofy-settings', 'studiofy_api', array( 'id' => 'google_client_id' ) );
-		add_settings_field( 'google_client_secret', __( 'Google Secret', 'studiofy-crm' ), array( $this, 'field_password' ), 'studiofy-settings', 'studiofy_api', array( 'id' => 'google_client_secret' ) );
-		add_settings_field( 'square_access_token', __( 'Square Token', 'studiofy-crm' ), array( $this, 'field_password' ), 'studiofy-settings', 'studiofy_api', array( 'id' => 'square_access_token' ) );
-		add_settings_field( 'square_location_id', __( 'Square Location ID', 'studiofy-crm' ), array( $this, 'field_text' ), 'studiofy-settings', 'studiofy_api', array( 'id' => 'square_location_id' ) );
+	public function menu() {
+		add_submenu_page( 'studiofy-dashboard', 'Settings', 'Settings', 'manage_options', 'studiofy-settings', array( $this, 'render' ) );
 	}
-
-	public function sanitize_callback( array $input ): array {
+	public function init() {
+		register_setting( 'studiofy_options', 'studiofy_settings', array( $this, 'sanitize' ) );
+		add_settings_section( 'api_keys', 'API Keys', null, 'studiofy-settings' );
+		add_settings_field( 'square_access_token', 'Square Token', array( $this, 'field_token' ), 'studiofy-settings', 'api_keys' );
+	}
+	public function sanitize( $input ) {
 		$new = array();
 		$enc = new Studiofy_Encryption();
-		$old = get_option( 'studiofy_settings', array() );
-
-		$new['google_client_id']   = sanitize_text_field( $input['google_client_id'] ?? '' );
-		$new['square_location_id'] = sanitize_text_field( $input['square_location_id'] ?? '' );
-
-		if ( ! empty( $input['google_client_secret'] ) ) {
-			$new['google_client_secret'] = $enc->encrypt( $input['google_client_secret'] );
-		} else {
-			$new['google_client_secret'] = $old['google_client_secret'] ?? '';
-		}
-
-		if ( ! empty( $input['square_access_token'] ) ) {
-			$new['square_access_token'] = $enc->encrypt( $input['square_access_token'] );
-		} else {
-			$new['square_access_token'] = $old['square_access_token'] ?? '';
-		}
-
+		if ( ! empty( $input['square_access_token'] ) ) $new['square_access_token'] = $enc->encrypt( $input['square_access_token'] );
 		return $new;
 	}
-
-	public function field_text( array $args ): void {
-		$opts  = get_option( 'studiofy_settings', array() );
-		$id    = $args['id'] ?? '';
-		$value = $opts[ $id ] ?? '';
-		echo '<input type="text" name="studiofy_settings[' . esc_attr( $id ) . ']" value="' . esc_attr( $value ) . '" class="regular-text">';
+	public function field_token() {
+		echo '<input type="password" name="studiofy_settings[square_access_token]" class="regular-text" placeholder="Encrypted">';
 	}
-
-	public function field_password( array $args ): void {
-		$id = $args['id'] ?? '';
-		echo '<input type="password" name="studiofy_settings[' . esc_attr( $id ) . ']" placeholder="' . esc_attr__( 'Encrypted', 'studiofy-crm' ) . '" class="regular-text">';
+	public function render() {
+		echo '<div class="wrap"><h1>Settings</h1><form method="post" action="options.php">';
+		settings_fields( 'studiofy_options' );
+		do_settings_sections( 'studiofy-settings' );
+		submit_button();
+		echo '</form></div>';
 	}
 }
