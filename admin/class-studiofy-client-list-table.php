8428c84f3d19a9c381d2b1fcafbd5025fb9020cd
Ian <118707480+irgordon@users.noreply.github.com>
2025-12-03 20:33:16 -0500

    Create class-studiofy-client-list-table.php

---
+++ admin/class-studiofy-client-list-table.php
@@ -0,0 +1,26 @@
+<?php
+if ( ! class_exists( 'WP_List_Table' ) ) require_once( ABSPATH . 'wp-admin/includes/class-wp-list-table.php' );
+
+class Studiofy_Client_List_Table extends WP_List_Table {
+    public function prepare_items() {
+        $this->_column_headers = array( $this->get_columns(), array(), array() );
+        $this->items = $this->get_data();
+    }
+
+    public function get_columns() {
+        return array( 'name' => 'Name', 'email' => 'Email', 'status' => 'Status' );
+    }
+
+    private function get_data() {
+        global $wpdb;
+        return $wpdb->get_results( "SELECT * FROM {$wpdb->prefix}studiofy_clients ORDER BY created_at DESC", ARRAY_A );
+    }
+
+    public function column_default( $item, $column_name ) {
+        return esc_html( $item[ $column_name ] );
+    }
+    
+    public function column_status( $item ) {
+        return '<span class="studiofy-badge status-'.esc_attr($item['status']).'">'.esc_html(ucfirst($item['status'])).'</span>';
+    }
+}
