848a14c0f555ea1d000a9a21d0297e67d4bee893
Ian <118707480+irgordon@users.noreply.github.com>
2025-12-03 21:06:51 -0500

    Update class-studiofy-google-calendar.php

--- includes/integrations/class-studiofy-google-calendar.php
+++ includes/integrations/class-studiofy-google-calendar.php
@@ -3,42 +3,42 @@ use Google\Client;
 use Google\Service\Calendar;
 
 class Studiofy_Google_Calendar {
-    private $client;
-    private $encryption;
-    
-    public function __construct() {
-        require_once STUDIOFY_PATH . 'includes/utils/class-encryption.php';
-        $this->encryption = new Studiofy_Encryption();
-        $this->init_client();
-    }
+	private $client;
+	private $encryption;
 
-    private function init_client() {
-        $options = get_option( 'studiofy_settings' );
-        
-        $this->client = new Client();
-        $this->client->setApplicationName( 'Studiofy CRM' );
-        $this->client->setScopes( Calendar::CALENDAR_EVENTS );
-        $this->client->setAccessType( 'offline' );
-        $this->client->setPrompt( 'select_account consent' );
+	public function __construct() {
+		require_once STUDIOFY_PATH . 'includes/utils/class-encryption.php';
+		$this->encryption = new Studiofy_Encryption();
+		$this->init_client();
+	}
 
-        $client_id = isset( $options['google_client_id'] ) ? $options['google_client_id'] : '';
-        $encrypted_secret = isset( $options['google_client_secret'] ) ? $options['google_client_secret'] : '';
-        $client_secret = $this->encryption->decrypt( $encrypted_secret );
+	private function init_client() {
+		$options = get_option( 'studiofy_settings' );
 
-        if ( $client_id && $client_secret ) {
-            $this->client->setClientId( $client_id );
-            $this->client->setClientSecret( $client_secret );
-            $this->client->setRedirectUri( admin_url( 'admin.php?page=studiofy-settings' ) );
-        }
-    }
-    
-    public function get_auth_url() {
-        return $this->client->createAuthUrl();
-    }
+		$this->client = new Client();
+		$this->client->setApplicationName( 'Studiofy CRM' );
+		$this->client->setScopes( Calendar::CALENDAR_EVENTS );
+		$this->client->setAccessType( 'offline' );
+		$this->client->setPrompt( 'select_account consent' );
 
-    public function is_connected() {
-        // Logic to check stored access token presence
-        $token = get_option('studiofy_google_token');
-        return !empty($token);
-    }
+		$client_id = isset( $options['google_client_id'] ) ? $options['google_client_id'] : '';
+		
+		$encrypted_secret = isset( $options['google_client_secret'] ) ? $options['google_client_secret'] : '';
+		$client_secret    = $this->encryption->decrypt( $encrypted_secret );
+
+		if ( $client_id && $client_secret ) {
+			$this->client->setClientId( $client_id );
+			$this->client->setClientSecret( $client_secret );
+			$this->client->setRedirectUri( admin_url( 'admin.php?page=studiofy-settings' ) );
+		}
+	}
+
+	public function get_auth_url() {
+		return $this->client->createAuthUrl();
+	}
+
+	public function is_connected() {
+		$token = get_option( 'studiofy_google_token' );
+		return ! empty( $token );
+	}
 }
