a361c58187398baad382ca47d1eb441a3eac8798
Ian <118707480+irgordon@users.noreply.github.com>
2025-12-03 20:43:19 -0500

    Create class-studiofy-google-calendar.php

---
+++ includes/integrations/class-studiofy-google-calendar.php
@@ -0,0 +1,40 @@
+<?php
+use Google\Client;
+use Google\Service\Calendar;
+
+class Studiofy_Google_Calendar {
+
+    private $client;
+    private $encryption;
+    
+    public function __construct() {
+        require_once STUDIOFY_PATH . 'includes/utils/class-encryption.php';
+        $this->encryption = new Studiofy_Encryption();
+        $this->init_client();
+    }
+
+    private function init_client() {
+        $options = get_option( 'studiofy_settings' );
+        
+        $this->client = new Client();
+        $this->client->setApplicationName( 'Studiofy CRM User Install' );
+        $this->client->setScopes( Calendar::CALENDAR_EVENTS );
+        $this->client->setAccessType( 'offline' );
+        $this->client->setPrompt( 'select_account consent' );
+
+        // Pull Dynamic Keys
+        $client_id = isset( $options['google_client_id'] ) ? $options['google_client_id'] : '';
+        
+        // Decrypt Secret
+        $encrypted_secret = isset( $options['google_client_secret'] ) ? $options['google_client_secret'] : '';
+        $client_secret = $this->encryption->decrypt( $encrypted_secret );
+
+        if ( $client_id && $client_secret ) {
+            $this->client->setClientId( $client_id );
+            $this->client->setClientSecret( $client_secret );
+            $this->client->setRedirectUri( admin_url( 'admin.php?page=studiofy-settings' ) );
+        }
+    }
+    
+    // ... (Rest of logic: is_connected, get_auth_url, etc. remains the same) ...
+}
