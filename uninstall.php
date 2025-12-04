b034817a6fcd2644c30c93597ca8d1aa1717f7f2
Ian <118707480+irgordon@users.noreply.github.com>
2025-12-04 04:05:13 -0500

    Update uninstall.php

--- uninstall.php
+++ uninstall.php
@@ -1,13 +1,11 @@
 <?php
 if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) exit;
-
 $options = get_option( 'studiofy_settings' );
-
 if ( isset( $options['delete_data_on_uninstall'] ) && 'true' === $options['delete_data_on_uninstall'] ) {
 	$cpts = array( 'studiofy_project', 'studiofy_lead', 'studiofy_invoice', 'studiofy_contract', 'studiofy_session', 'studiofy_gallery' );
-	foreach ( $cpts as $post_type ) {
-		$posts = get_posts( array( 'post_type' => $post_type, 'numberposts' => -1, 'post_status' => 'any' ) );
-		foreach ( $posts as $post ) wp_delete_post( $post->ID, true );
+	foreach ( $cpts as $pt ) {
+		$posts = get_posts( array( 'post_type' => $pt, 'numberposts' => -1, 'post_status' => 'any' ) );
+		foreach ( $posts as $p ) wp_delete_post( $p->ID, true );
 	}
 	global $wpdb;
 	$wpdb->query( "DROP TABLE IF EXISTS {$wpdb->prefix}studiofy_clients" );
