237b99cae6114e587e017e160a61e65dbcc5ed3e
Ian <118707480+irgordon@users.noreply.github.com>
2025-12-05 15:46:54 -0500

    Update admin.js

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -1,5 +1,24 @@
 /**
- * Studiofy Admin
- * @version 2.0.0
+ * Studiofy Admin Core
+ * @package Studiofy
+ * @version 2.0.4
  */
-jQuery(document).ready(function($){ $('.studiofy-color-field').wpColorPicker(); });
+jQuery(document).ready(function($){
+    // Color Picker
+    $('.studiofy-color-field').wpColorPicker();
+
+    // Media Uploader
+    $('.studiofy-upload-btn').click(function(e) {
+        e.preventDefault();
+        var button = $(this);
+        var targetId = button.data('target');
+        var custom_uploader = wp.media({
+            title: 'Select Image',
+            button: { text: 'Use this image' },
+            multiple: false
+        }).on('select', function() {
+            var attachment = custom_uploader.state().get('selection').first().toJSON();
+            $(targetId).val(attachment.url);
+        }).open();
+    });
+});
