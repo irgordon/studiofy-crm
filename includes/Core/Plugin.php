b6c81aa0eba3de2989cdca10b0331ddfa1974840
Ian <118707480+irgordon@users.noreply.github.com>
2025-12-05 00:21:53 -0500

    Create Plugin.php

---
+++ includes/Core/Plugin.php
@@ -0,0 +1,27 @@
+<?php
+declare(strict_types=1);
+namespace Studiofy\Core;
+
+use Studiofy\Admin\Menu;
+use Studiofy\Admin\GalleryController;
+use Studiofy\Api\ClientRoutes;
+use Studiofy\Api\BookingRoutes;
+use Studiofy\Api\KanbanRoutes;
+use Studiofy\Api\ProjectEndpoints;
+use Studiofy\Api\GalleryRoutes;
+use Studiofy\Api\InvoiceRoutes;
+
+class Plugin {
+    public function run(): void {
+        (new Menu())->init();
+        (new GalleryController())->init();
+        
+        // Init APIs
+        (new ClientRoutes())->init();
+        (new BookingRoutes())->init();
+        (new KanbanRoutes())->init();
+        (new ProjectEndpoints())->init();
+        (new GalleryRoutes())->init();
+        (new InvoiceRoutes())->init();
+    }
+}
