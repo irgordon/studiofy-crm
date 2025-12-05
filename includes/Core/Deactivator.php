49bf3af053aff70bf4d76903dddbcb1622616494
Ian <118707480+irgordon@users.noreply.github.com>
2025-12-05 15:33:23 -0500

    Update Deactivator.php

--- includes/Core/Deactivator.php
+++ includes/Core/Deactivator.php
@@ -1,4 +1,16 @@
 <?php
+/**
+ * Deactivator
+ * @package Studiofy\Core
+ * @version 2.0.4
+ */
+
 declare(strict_types=1);
+
 namespace Studiofy\Core;
-class Deactivator { public static function deactivate(): void { flush_rewrite_rules(); } }
+
+class Deactivator {
+    public static function deactivate(): void {
+        flush_rewrite_rules();
+    }
+}
