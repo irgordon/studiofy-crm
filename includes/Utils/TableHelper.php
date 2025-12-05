ce5f3f07065aeb7eb7eaa357c00392c3e9950fdb
Ian <118707480+irgordon@users.noreply.github.com>
2025-12-05 15:43:04 -0500

    Update TableHelper.php

--- includes/Utils/TableHelper.php
+++ includes/Utils/TableHelper.php
@@ -2,10 +2,27 @@
 /**
  * Table Helper
  * @package Studiofy\Utils
- * @version 2.0.0
+ * @version 2.0.4
  */
+
 declare(strict_types=1);
+
 namespace Studiofy\Utils;
+
 trait TableHelper {
-    private function sort_link(string $n, string $s): string { return "<a href='?orderby=$s'>$n</a>"; }
+    private function sort_link(string $col_name, string $col_slug, string $default_order = 'asc'): string {
+        $current_orderby = $_GET['orderby'] ?? '';
+        $current_order   = $_GET['order'] ?? 'asc';
+        
+        $next_order = ($current_orderby === $col_slug && $current_order === 'asc') ? 'desc' : 'asc';
+        $arrow = '';
+
+        if ($current_orderby === $col_slug) {
+            $arrow = ($current_order === 'asc') ? ' &uarr;' : ' &darr;';
+        }
+
+        $url = add_query_arg(['orderby' => $col_slug, 'order' => $next_order]);
+        
+        return "<a href='" . esc_url($url) . "'><span>$col_name</span> $arrow</a>";
+    }
 }
