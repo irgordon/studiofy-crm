4049aee17aa27ce5b7dc08e5bafd8a6a3eeed988
Ian <118707480+irgordon@users.noreply.github.com>
2025-12-05 15:32:44 -0500

    Update Plugin.php
    v2.0.4

--- includes/Core/Plugin.php
+++ includes/Core/Plugin.php
@@ -1,5 +1,12 @@
 <?php
+/**
+ * Main Controller
+ * @package Studiofy\Core
+ * @version 2.0.4
+ */
+
 declare(strict_types=1);
+
 namespace Studiofy\Core;
 
 use Studiofy\Admin\Menu;
@@ -12,9 +19,15 @@ use Studiofy\Api\GalleryRoutes;
 use Studiofy\Api\InvoiceRoutes;
 
 class Plugin {
+    
     public function run(): void {
+        // Admin Menu & UI
         (new Menu())->init();
+        
+        // Modules
         (new GalleryController())->init();
+        
+        // REST APIs
         (new ClientRoutes())->init();
         (new BookingRoutes())->init();
         (new KanbanRoutes())->init();
