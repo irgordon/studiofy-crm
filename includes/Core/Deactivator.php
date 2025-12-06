f39e3eee4c0d7e9807c722961b407d37552dabc8
Ian <118707480+irgordon@users.noreply.github.com>
2025-12-06 11:23:39 -0500

    Update Deactivator.php

--- includes/Core/Deactivator.php
+++ includes/Core/Deactivator.php
@@ -2,7 +2,7 @@
 /**
  * Deactivator
  * @package Studiofy\Core
- * @version 2.1.12
+ * @version 2.2.9
  */
 
 declare(strict_types=1);
@@ -10,7 +10,15 @@ declare(strict_types=1);
 namespace Studiofy\Core;
 
 class Deactivator {
+    
     public static function deactivate(): void {
+        // Check if Demo Data exists
+        if (get_option('studiofy_demo_data_ids')) {
+            // Instantiate Manager to clean up
+            $demoManager = new DemoDataManager();
+            $demoManager->delete_demo_data();
+        }
+
         flush_rewrite_rules();
     }
 }
