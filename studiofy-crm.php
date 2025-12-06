f10f870dd67408b038a69bb3d659ac08ae7f7f12
Ian <118707480+irgordon@users.noreply.github.com>
2025-12-06 11:08:24 -0500

    Update studiofy-crm.php

--- studiofy-crm.php
+++ studiofy-crm.php
@@ -2,14 +2,14 @@
 /**
  * Plugin Name: Studiofy CRM
  * Description: A comprehensive Elementor Addon and CRM for Photographers.
- * Version: 2.2.6
+ * Version: 2.2.7
  * Author: Ian R. Gordon
  * Text Domain: studiofy
  * Requires PHP: 8.1
  * Requires at least: 6.6
  * Elementor tested up to: 3.25.0
  * @package Studiofy
- * @version 2.2.6
+ * @version 2.2.7
  */
 
 declare(strict_types=1);
@@ -20,7 +20,7 @@ if (!defined('ABSPATH')) {
     exit;
 }
 
-define('STUDIOFY_VERSION', '2.2.6');
+define('STUDIOFY_VERSION', '2.2.7');
 define('STUDIOFY_DB_VERSION', '2.12');
 define('STUDIOFY_PATH', plugin_dir_path(__FILE__));
 define('STUDIOFY_URL', plugin_dir_url(__FILE__));
